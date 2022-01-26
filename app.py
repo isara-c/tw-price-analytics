58ff8565844695e64ed307c0c029ebd1567ab129
isara-c <98464634+isara-c@users.noreply.github.com>
2022-01-26 19:38:22 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+# import pyodbc
+
+# server = 'siametrices-db.database.windows.net'
+# database = 'siametricesdb' 
+# username = 'siametricesdbadmin'
+# password = 'Twpc@2022pM!!dB'
+
+# # Specifying the ODBC driver, server name, database, etc. directly
+# cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=siametrices-db.database.windows.net;DATABASE=siametrices-db;UID=siametricesdbadmin;PWD=Twpc@2022pM!!dB')
+
+# # Create a cursor from the connection
+# cursor = cnxn.cursor()
+
+with open('test-smc-app.py','w',encoding = 'utf-8') as f:
+   f.write("test-smc-app, done.")
